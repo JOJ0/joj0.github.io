cfdcb4d78b81f62a0dec7e7206c212c369e8a1ab
J0J0 T <jojo@peek-a-boo.at>
2018-05-30 20:28:26 +0200

    mpc1k-seq: make readme-update script more conveniant

--- update-mpc1k-seq.sh
+++ update-mpc1k-seq.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+echo just copying README.md...
 tail -n+4 ../seq/README.md > _posts/mpc1k-seq_README.md
-git add _posts/mpc1k-seq_README.md 
-git commit -m "mpc1k-seq_README.md update"
-echo "push with:"
-echo "git push"
+
+if [ "$1" = "doit"  ]; then
+  echo "commiting and pushing..."
+  git add _posts/mpc1k-seq_README.md 
+  git commit -m "mpc1k-seq_README.md update"
+  git push
+else
+  echo "to also commit and push, add argument \"doit\""
+fi
